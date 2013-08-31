8e42533928ba5f0ee2d200353c134fb61d6e19d3
Jim R. Wilson <wilson.jim.r@gmail.com>
2013-08-30 21:34:36 -0400

    basic availablity test ready, now to make it pass

--- test/test-available.js
+++ test/test-available.js
@@ -1,44 +1,14 @@
 /**
- * test.js - a couple of unit tests for intricate functionality.
+ * test-available.js - basic availability test.
  */
-testMerge = corridor.testMerge = function() {
-  ([
-    {
-      obj: ['a'],
-      other: ['b'],
-      expected: ['a', 'b']
-    },
-    {
-      obj: [{a: 'hi'}],
-      other: [{b: 'there'}],
-      expected: [{a: 'hi'}, {b: 'there'}]
-    },
-    {
-      obj: {list: ['hi']},
-      other: {list: ['there']},
-      expected: {list: ['hi','there']}
-    },
-    {
-      obj: {list: ['hi'], foo: 7},
-      other: {foo: 8, list: ['there']},
-      expected: {list: ['hi','there'], foo: 8}
-    },
-    {
-      obj: {},
-      other: { b: 'hi' },
-      expected: { b: 'hi' }
-    },
-    {
-      obj: { a: 'whut' },
-      other: { b: 'hi' },
-      expected: { a: 'whut', b: 'hi' }
-    },
-  ]).forEach(function(test) {
-    var actual = merge(test.obj, test.other);
-    if (JSON.stringify(actual) !== JSON.stringify(test.expected)) {
-      console.log(["FAILED", test.expected, actual]);
-    }
-  });
-  console.log("PASS");
-};
 
+var corridor = require('../src/corridor.js');
+
+exports.testAvailable = function(test) {
+  
+  test.expect(2);
+  test.ok(corridor, 'corridor exists');
+  test.equals(Object.prototype.toString.call(corridor), '[object Function]', 'corridor is a function');
+  test.done();
+  
+};
