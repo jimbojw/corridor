2d4d7f934ba7eae41b636111a05e4c2b11205b1c
Jim R. Wilson <wilson.jim.r@gmail.com>
2013-09-03 17:02:42 -0400

    adding test for intelligent merging of objects

--- test/test-merge.js
+++ test/test-merge.js
@@ -1,7 +1,38 @@
 /**
  * test-merge.js - tests the merge() function.
  */
-exports['corridor.merge()'] = function(test) {
+exports['corridor.merge(objects)'] = function(test) {
+  
+  var
+    
+    corridor = require('../src/corridor.js'),
+    
+    suite = [{
+      obj: {},
+      other: { b: 'hi' },
+      expected: { b: 'hi' },
+      reason: 'all keys should be added to empty objects'
+    },{
+      obj: { a: 'whut' },
+      other: { b: 'hi' },
+      expected: { a: 'whut', b: 'hi' },
+      reason: 'missing keys should be added to non-empty objects'
+    }];
+  
+  test.expect(suite.length);
+  
+  for (var i = 0, ii = suite.length; i < ii; i++) {
+    (function(data){
+      var actual = corridor.merge(data.obj, data.other);
+      test.deepEqual(actual, data.expected, data.reason);
+    })(suite[i]);
+  }
+  
+  test.done();
+  
+};
+
+exports['corridor.merge(arrays)'] = function(test) {
   
   var
     
@@ -14,9 +45,14 @@ exports['corridor.merge()'] = function(test) {
       reason: 'arrays of primitives should concatenate'
     },{
       obj: [{a: 'hi'}],
+      other: [{a: 'there'}],
+      expected: [{a: 'hi'}, {a: 'there'}],
+      reason: 'arrays of conflicting objects should concatenate'
+    },{
+      obj: [{a: 'hi'}],
       other: [{b: 'there'}],
-      expected: [{a: 'hi'}, {b: 'there'}],
-      reason: 'arrays of objects should concatenate'
+      expected: [{a: 'hi', b: 'there'}],
+      reason: 'arrays of non-conflicting objects should merge'
     },{
       obj: {list: ['hi']},
       other: {list: ['there']},
@@ -27,16 +63,6 @@ exports['corridor.merge()'] = function(test) {
       other: {foo: 8, list: ['there']},
       expected: {list: ['hi','there'], foo: 8},
       reason: 'primitves should overwrite each other while arrays concatenate'
-    },{
-      obj: {},
-      other: { b: 'hi' },
-      expected: { b: 'hi' },
-      reason: 'all keys should be added to empty objects'
-    },{
-      obj: { a: 'whut' },
-      other: { b: 'hi' },
-      expected: { a: 'whut', b: 'hi' },
-      reason: 'missing keys should be added to non-empty objects'
     }];
   
   test.expect(suite.length);
