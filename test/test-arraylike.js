59cbe261d9e0758b32c3bbea137f66c45ce01b70
Jim R. Wilson <wilson.jim.r@gmail.com>
2013-09-03 23:17:36 -0400

    adding arraylike() unit tests

---
+++ test/test-arraylike.js
@@ -0,0 +1,67 @@
+/**
+ * test-arraylike.js - tests the arraylike() function.
+ */
+exports['corridor.arraylike()'] = function(test) {
+  
+  var
+    
+    corridor = require('../src/corridor.js'),
+    
+    suite = [{
+      obj: 'hi',
+      expected: false,
+      reason: 'a primitive value is not array-like'
+    },{
+      obj: ['hi'],
+      expected: true,
+      reason: 'an array is array-like'
+    },{
+      obj: {"foo":"bar"},
+      expected: false,
+      reason: 'an object with non-integer keys other than length is not array-like'
+    },{
+      obj: {},
+      expected: true,
+      reason: 'an empty object is array-like'
+    },{
+      obj: {"1":"foo"},
+      expected: false,
+      reason: 'an object with one element must contain a 0\'th element'
+    },{
+      obj: {"0":"bar"},
+      expected: true,
+      reason: 'an object with a 0\`th element is array-like'
+    },{
+      obj: {"1":"foo","0":"bar"},
+      expected: true,
+      reason: 'an object with a 0\`th element is array-like, even with out-of-order keys'
+    },{
+      obj: {"length":0},
+      expected: true,
+      reason: 'an otherwise empty object with a length property set to 0 is array-like'
+    },{
+      obj: {"length":2},
+      expected: false,
+      reason: 'an otherwise empty object with a non-zero length property is not array-like'
+    },{
+      obj: {"1":"foo","length":2,"0":"bar"},
+      expected: true,
+      reason: 'an object with a correctly specified length is array-like'
+    }];
+  
+  test.expect(suite.length + 1);
+  
+  test.equals(toString.call(corridor.arraylike), '[object Function]', 'arraylike() should be a corridor method');
+  
+  for (var i = 0, ii = suite.length; i < ii; i++) {
+    (function(data){
+      var actual = corridor.arraylike(data.obj);
+      test.equals(actual, data.expected, data.reason);
+    })(suite[i]);
+  }
+  
+  test.done();
+  
+};
+
+
